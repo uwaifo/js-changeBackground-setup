f656aaa73875af00793ca931f36c3de5fd7e17d1
Uwaifo Idehenre <mr.uwaifo@gmail.com>
2019-03-15 03:55:10 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
+//an array of colrs
+const colors = ["yellow", "blue", "green", "red", "purple", "white"];
+//
+const btn = document.getElementById("btn");
+//watchout for change on element btn
+btn.addEventListener("click", () => {
+  let random = Math.floor(Math.random() * colors.length);
 
+  const body = document.body;
+  body.style.backgroundColor = colors[random];
+
+  console.log(random, colors[random]);
+});
